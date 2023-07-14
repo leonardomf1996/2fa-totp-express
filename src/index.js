56d66c1f2674a2d07991107c732e5274cb905463
Leonardo Furtado <leonardomf1996@gmail.com>
2023-07-14 13:59:08 -0300

    feat: first commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const speakeasy = require('speakeasy')
+const bodyparser = require('body-parser')
+
+const app = express();
+
+const STEP = 60;
+const WINDOW = 3;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.post('/totp-secret', (request, response, next) => {
+   const secret = speakeasy.generateSecret({ length: 20 });
+   response.send({ secret: secret.base32 })
+})
+
+app.post('/totp-generate', (request, response, next) => {
+   response.send({
+      token: speakeasy.totp({
+         secret: request.body.secret,
+         encoding: 'base32',
+         step: STEP, 
+         window: WINDOW 
+      }),
+      remaining: (30 - Math.floor((new Date().getTime() / 1000.0 % 30)))
+   })
+})
+
+app.post('/totp-validate', (request, response, next) => {
+   response.send({
+      valid: speakeasy.totp.verify({
+         secret: request.body.secret,
+         encoding: 'base32',
+         token: request.body.token,
+         step: STEP,
+         window: WINDOW
+      })
+   })
+})
+
+app.listen(3000, () => {
+   console.log('HTTP server running on port 3000')
+})
